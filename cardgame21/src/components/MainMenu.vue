4cd2b04459844c29d6066ee06153a50ab5f12fae
Jenech WW <jenech.2121@gmail.com>
2026-04-07 08:49:50 +0300

    1

---
+++ cardgame21/src/components/MainMenu.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+  32e
+</template>
+
---
+++ cardgame21/src/views/MainGame.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <MainMenu>
+    <template>
+      <div>
+      332dewd
+      </div>
+    </template>
+  </MainMenu>
+
+</template>
