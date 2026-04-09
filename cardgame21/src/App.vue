103bb6cfb7feeaf4ca1ab7dbc8c7a67b1a215b34
Jenech WW <jenech.2121@gmail.com>
2026-04-09 17:48:04 +0300

    minimum changes

--- cardgame21/src/App.vue
+++ cardgame21/src/App.vue
@@ -1,87 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import MainMenu from './components/MainMenu.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/MainMenu">Main </RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <div class="bg-black min-h-screen">
+ <div class="flex justify-center ">
+   <RouterLink class="flex items-center justify-center space-x-3 w-full h-full min-h-screen text-center text-white  text-7xl md:text-6xl font-extrabold tracking-wide hover:scale-110 transition-transform animate-pulse [text-shadow:0_0_30px_#fff,0_0_60px_#fff]  ">ИГРАТЬ</RouterLink> 
+  </div>
   <RouterView />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
